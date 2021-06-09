609f9d342c11a95f76ce1ad0f228fadac76e8291
ktgw0316 <ktgw0316@users.noreply.github.com>
2021-06-09 11:23:23 +0900

    Update 0012.kts

--- src/main/kotlin/0012.kts
+++ src/main/kotlin/0012.kts
@@ -1,17 +1,33 @@
 // http://odz.sakura.ne.jp/projecteuler/?Problem+12
+// cf. https://freak-da.hatenablog.com/entry/20100729/p1
 
-val triangles = sequence<Int> {
-    var curr = 1
-    var next = 2
-    while (true) {
-        yield(curr)
-        curr += next
-        next++
-    }
+import java.util.concurrent.ConcurrentHashMap
+
+val memo = ConcurrentHashMap<Int, Int?>()
+
+fun divisorSize(n: Int): Int {
+    return memo.computeIfAbsent(n) {
+        // val r = Math.sqrt(n.toDouble()).toInt()
+        // if (n == r * (r + 1)) {
+        //     divisorSize(r) * divisorSize(r + 1)
+        // } else {
+            2 + (2..n / 2).filter { n % it == 0 }.size
+        // }
+    }!!
 }
 
-assert(triangles.take(7).last() == 28)
+fun triangleDivisorSize(i: Int): Pair<Int, Int> {
+    val (a, b) = when {
+        i % 2 == 0 -> i / 2 to i + 1
+        else -> i to (i+1) / 2
+    }
+    return a * b to divisorSize(a) * divisorSize(b)
+}
 
-fun Int.divisorSize() = 2 + (2..this / 2).filter { this % it == 0 }.size
+assert(triangleDivisorSize(7) == 28 to 6)
 
-println(triangles.first { it.divisorSize() > 500 })
+val ans = (1..Int.MAX_VALUE).asSequence()
+        .map(::triangleDivisorSize)
+        .first { it.second > 500 }
+        .first
+println(ans)

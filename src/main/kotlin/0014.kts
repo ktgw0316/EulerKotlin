89b98b6984e38f1f563d125b8aa903dba9783c69
Masahiro Kitagawa <arctica0316@gmail.com>
2021-06-12 22:40:27 +0900

    Update 0014.kts

--- src/main/kotlin/0014.kts
+++ src/main/kotlin/0014.kts
@@ -1,23 +1,34 @@
 // http://odz.sakura.ne.jp/projecteuler/?Problem+14
 
-import kotlin.math.floor
-import kotlin.math.log2
+import java.util.concurrent.ConcurrentHashMap
 
-fun Double.isInteger() = floor(this) == this
+// val memo: Array<Int?> = arrayOfNulls(1_000_000)
+//
+// tailrec fun collatzLengthMemoised(m: Long, n: Long = m, curr: Int = 0): Int {
+//     return when {
+//         n < memo.size && memo[n.toInt()] != null -> curr + memo[n.toInt()]!!
+//         n == 1L -> {
+//             if (m < memo.size.toLong()) memo[m.toInt()] = curr + 1
+//             curr + 1
+//         }
+//         n % 2 == 0L -> collatzLengthMemoised(m, n / 2, curr + 1)
+//         else -> collatzLengthMemoised(m, 3 * n + 1, curr + 1)
+//     }
+// }
 
-tailrec fun collatzLength(n: Int, curr: Int = 1): Int {
-    val l = log2(n.toDouble())
+tailrec fun collatzLength(n: Long, curr: Int = 0): Int {
     return when {
-        l.isInteger() -> curr + l.toInt() // n == 2^l
-        n % 2 == 0 -> collatzLength(n / 2, curr + 1)
+        n == 1L -> curr + 1
+        n % 2 == 0L -> collatzLength(n / 2, curr + 1)
         else -> collatzLength(3 * n + 1, curr + 1)
     }
 }
 
-assert(collatzLength(13) == 10)
+assert(collatzLength(13L) == 10)
+assert(collatzLength(7L) == 17)
 
 println(
-    (1 until 1_000_000).asSequence()
+    (1L until 1_000_000L)
         .map { collatzLength(it) }
         .maxOrNull()
 )

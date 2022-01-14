dcc7a57a777ffbe6e96957c7a268abc965502229
Masahiro Kitagawa <arctica0316@gmail.com>
2022-01-14 16:31:44 +0900

    Refactor 0017

--- src/main/kotlin/0017.kts
+++ src/main/kotlin/0017.kts
@@ -1,36 +1,69 @@
 // http://www.odz.sakura.ne.jp/projecteuler/?Problem+17
+// ISBN978-4-04-893053-6 の 1.4 節を参照
 
-val dictStr1 = mapOf(
-        0 to "", 1 to "one", 2 to "two", 3 to "three", 4 to "four",
-        5 to "five", 6 to "six", 7 to "seven", 8 to "eight", 9 to "nine",
-        10 to "ten", 11 to "eleven", 12 to "twelve", 13 to "thirteen",
-        14 to "fourteen")  // not "forteen"
-val dictStr2 = mapOf(
-        0 to "", 1 to "", 2 to "twen", 3 to "thir", 4 to "for",
-        5 to "fif", 6 to "six", 7 to "seven", 8 to "eigh", 9 to "nine")
+val units = listOf("zero", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine")
 
-fun write(n: Int): String {
-    val n1 = n - n / 10 * 10
-    val n2 = n / 10 - n / 100 * 10
-    val n3 = n / 100 - n / 1000 * 10
-    val n4 = n / 1000
-    var str = ""
-    if (n4 != 0) str += dictStr1[n4] + " thousand "
-    if (n3 != 0) str += dictStr1[n3] + " handred"
-    if (n2 == 0 && n1 == 0) return str
-    if (n4 != 0 || n3 != 0) str += " and "
-    val n21 = 10 * n2 + n1
-    str += when {
-        n21 <= 14 -> dictStr1[n21]
-        n21 <= 19 -> dictStr2[n1] + "teen"
-        else -> dictStr2[n2] + "ty " + dictStr1[n1]
+val teens = listOf("ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
+
+val tens = listOf("twenty", "thirty", "forty", "fifty",
+        "sixty", "seventy", "eighty", "ninety")
+
+fun convert1(n: Int): String = units[n]
+
+//fun digit2(n: Int): Pair<Int, Int> = pairOf(n / 10, n % 10)
+//fun convert2(n: Int): String = convine2(digit2(n))
+//fun conbine2(p: Pair<Int, Int>): String = conbine2(p.first, p.second)
+//fun conbine2(t: Int, u: Int): String = when {
+//    t == 0 -> units[u]
+//    t == 1 -> teens[u]
+//    u == 0 -> tens[t]
+//    else -> tens[t] + "-" + units[u]
+//}
+
+fun convert2(n: Int): String {
+    val t = n / 10
+    val u = n % 10
+    return when {
+        t == 0 -> units[u]
+        t == 1 -> teens[u]
+        u == 0 -> tens[t - 2]
+        else -> tens[t - 2] + "-" + units[u]
     }
-    return str
 }
 
-(1..1000)
+fun convert3(n: Int): String {
+    val h = n / 100
+    val t = n % 100
+    return when {
+        h == 0 -> convert2(t)
+        t == 0 -> units[h] + " handred"
+        else -> units[h] + " handred and " + convert2(t)
+    }
+}
+
+fun convert(n: Int): String {
+    val k = n / 1000
+    val h = n % 1000
+    return when {
+        k == 0 -> convert3(h)
+        h == 0 -> convert3(k) + " thausand"
+        else -> convert3(k) + " thausand" + link(h) + convert3(h)
+    }
+}
+
+fun link(h: Int): String = if (h < 100) " and " else " "
+
+fun write(n: Int): String = convert(n)
+
+// println(write(342))
+// println(write(115))
+
+println((1..1000)
         .map(::write)
         // .onEach { println(it) }  // test
-        .map { it.count { it != ' ' }}
+        .map { it.count { it != ' ' && it != '-' }}
         .sum()
         .also { assert(it == 21124) }
+)
